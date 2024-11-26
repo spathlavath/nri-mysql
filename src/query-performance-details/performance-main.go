aba850a814ad87021c726d07dbe3ae479a3cedcd
tharun0064 <tbalanagu@newrelic.com>
2024-11-26 12:30:58 +0530

    logs refactor

--- src/query-performance-details/performance-main.go
+++ src/query-performance-details/performance-main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/newrelic/nri-mysql/src/query-performance-details/performance-database"
 	query_details "github.com/newrelic/nri-mysql/src/query-performance-details/query-details"
 	"github.com/newrelic/nri-mysql/src/query-performance-details/validator"
+	log "github.com/sirupsen/logrus"
 )
 
 // main
@@ -18,7 +19,7 @@ func PopulateQueryPerformanceMetrics(args arguments.ArgumentList, e *integration
 	defer db.Close()
 	isPreConditionsPassed := validator.ValidatePreconditions(db)
 	if !isPreConditionsPassed {
-		fmt.Println("Preconditions failed. Exiting.")
+		log.Error("Preconditions failed. Exiting.")
 		return
 	}
 
@@ -26,27 +27,27 @@ func PopulateQueryPerformanceMetrics(args arguments.ArgumentList, e *integration
 
 	individualQueryDetails, individualQueryDetailsErr := query_details.PopulateIndividualQueryDetails(db, queryIdList, e, args)
 	if individualQueryDetailsErr != nil {
-		fmt.Errorf("Error populating individual query details: %v", individualQueryDetailsErr)
+		log.Errorf("Error populating individual query details: %v", individualQueryDetailsErr)
 		return
 	}
 	fmt.Println("Query Plan details collected successfully.", individualQueryDetails)
 
 	executionPlanMetrics, executionPlanMetricsErr := query_details.PopulateExecutionPlans(db, individualQueryDetails, e, args)
 	if executionPlanMetricsErr != nil {
-		fmt.Errorf("Error populating execution plan details: %v", executionPlanMetricsErr)
+		log.Errorf("Error populating execution plan details: %v", executionPlanMetricsErr)
 		return
 	}
 	fmt.Println("Execution plan details collected successfully.", executionPlanMetrics)
 
 	_, waitEventError := query_details.PopulateWaitEventMetrics(db, e, args)
 	if waitEventError != nil {
-		fmt.Errorf("Error populating wait event metrics: %v", waitEventError)
+		log.Errorf("Error populating wait event metrics: %v", waitEventError)
 		return
 	}
 
 	_, populateBlockingSessionMetricsError := query_details.PopulateBlockingSessionMetrics(db, e, args)
 	if populateBlockingSessionMetricsError != nil {
-		fmt.Errorf("Error populating blocking session metrics: %v", populateBlockingSessionMetricsError)
+		log.Errorf("Error populating blocking session metrics: %v", populateBlockingSessionMetricsError)
 		return
 	}
 
