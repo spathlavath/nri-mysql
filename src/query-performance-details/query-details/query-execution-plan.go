737db4186d44e3fdccf4f3ff0e3eb8da2ef8aedd
spathlavath <spathlavath@newrelic.com>
2024-12-03 16:59:33 +0530

    test

--- src/query-performance-details/query-details/query-execution-plan.go
+++ src/query-performance-details/query-details/query-execution-plan.go
@@ -146,7 +146,7 @@ func processExecutionMetricsIngestion(e *integration.Entity, args arguments.Argu
 		Value      interface{}
 		MetricType metric.SourceType
 	}{
-		"query_id":       {common_utils.GetStringValueSafe(metricObject["query_id"].(string)), metric.ATTRIBUTE},
+		"query_id":       {common_utils.GetStringValueSafe(metricObject["query_id"]), metric.ATTRIBUTE},
 		"query_text":     {common_utils.GetStringValueSafe(metricObject["query_text"]), metric.ATTRIBUTE},
 		"total_cost":     {common_utils.GetFloat64ValueSafe(metricObject["total_cost"]), metric.GAUGE},
 		"step_id":        {common_utils.GetInt64ValueSafe(metricObject["step_id"]), metric.GAUGE},
