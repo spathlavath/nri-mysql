0e16e33f7c0f738465c86bf4c558bfcfdb56fb1b
spathlavath <spathlavath@newrelic.com>
2024-12-05 18:47:04 +0530

    update

--- src/query-performance-details/query-details/query-execution-plan.go
+++ src/query-performance-details/query-details/query-execution-plan.go
@@ -33,32 +33,21 @@ type DBPerformanceEvent struct {
 
 func PopulateExecutionPlans(db performance_database.DataSource, queries []performance_data_model.QueryPlanMetrics, e *integration.Entity, args arguments.ArgumentList) ([]DBPerformanceEvent, error) {
 	var events []DBPerformanceEvent
-	ms := common_utils.CreateMetricSet(e, "MysqlQueryExecution", args)
-	ms.SetMetric("query_id_1", "aaaaa...1", metric.ATTRIBUTE)
 	for _, query := range queries {
 		fmt.Printf("Query: %v\n", query)
 		tableIngestionDataList := processExecutionPlanMetrics(db, query)
 		events = append(events, tableIngestionDataList...)
 	}
-
-	ms.SetMetric("query_id_2", "aaaaa...2", metric.ATTRIBUTE)
-
 	// fmt.Printf("Total events collected: %d\n", len(events))
 
 	// if len(events) == 0 {
 	// 	return make([]DBPerformanceEvent, 0), nil
 	// }
-
-	ms.SetMetric("query_id_3", "aaaaa...3", metric.ATTRIBUTE)
-
 	err := SetExecutionPlanMetrics(e, args, events)
 	if err != nil {
 		log.Error("Error setting execution plan metrics: %v", err)
 		return nil, err
 	}
-
-	ms.SetMetric("query_id_4", "aaaaa...4", metric.ATTRIBUTE)
-
 	return events, nil
 }
 
@@ -138,7 +127,7 @@ func extractMetricsFromJSONString(jsonString, queryID string, eventID uint64) ([
 func SetExecutionPlanMetrics(e *integration.Entity, args arguments.ArgumentList, metrics []DBPerformanceEvent) error {
 	fmt.Printf("Setting execution plan metrics for %d metrics\n", len(metrics))
 	ms := common_utils.CreateMetricSet(e, "MysqlQueryExecution", args)
-	ms.SetMetric("inside_set_metrics", 2, metric.GAUGE)
+	ms.SetMetric("inside_set_metricsss", 2, metric.GAUGE)
 	for i, metricObject := range metrics {
 		ms.SetMetric("inside_set_metrics_loop_tens", i+10, metric.GAUGE)
 
