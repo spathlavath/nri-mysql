3bc880b6372da9f226a8b88a0e6607801efa4021
spathlavath <spathlavath@newrelic.com>
2025-01-02 18:45:10 +0530

    lint issue fixes

--- src/query-performance-details/validator/pre-conditions.go
+++ src/query-performance-details/validator/pre-conditions.go
@@ -25,19 +25,19 @@ func ValidatePreconditions(db dbconnection.DataSource) error {
 
 	if !performanceSchemaEnabled {
 		logEnablePerformanceSchemaInstructions(db)
-		return fmt.Errorf("performance Schema is not enabled. skipping validation.")
+		return fmt.Errorf("performance schema is not enabled. skipping validation.")
 	}
 
 	// Check if essential consumers are enabled
 	errEssentialConsumers := checkEssentialConsumers(db)
 	if errEssentialConsumers != nil {
-		return fmt.Errorf("essential consumer check failed: %v", fmt.Errorf("%w", errEssentialConsumers))
+		return fmt.Errorf("essential consumer check failed: %w", errEssentialConsumers)
 	}
 
 	// Check if essential instruments are enabled
 	errEssentialInstruments := checkEssentialInstruments(db)
 	if errEssentialInstruments != nil {
-		return fmt.Errorf("essential instruments check failed: %v", fmt.Errorf("%w", errEssentialInstruments))
+		return fmt.Errorf("essential instruments check failed: %w", errEssentialInstruments)
 	}
 	return nil
 }
